f96f2854e0448138c959baab19803c624dfcc378
ZoraPhoenix <raisondetre2001@gmail.com>
2025-04-03 18:27:19 -0400

    Create class Games and Objects HollowKnight and Undertale

---
+++ Games.py
@@ -0,0 +1,14 @@
+class Games():
+    def __init__(self, name, genre, platform):
+        self.name = name
+        self.genre = genre
+        self.platform = platform
+
+    def print_game(self):
+        print(f"Game: {self.name}, Genre: {self.genre}, Platform: {self.platform}")
+
+HollowKnight = Games("Hollow Knight", "Metroidvania", "PC")
+Undertale = Games("Undertale", "RPG", "PC")
+
+HollowKnight.print_game()
+Undertale.print_game()
