7b9175914c05816bd616a45dc1ef03e91ba1d3d2
ZoraPhoenix <raisondetre2001@gmail.com>
2025-04-04 18:45:49 -0400

    Create new list with list comprehension

---
+++ l_comprehension.py
@@ -0,0 +1,4 @@
+numbers = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+# Using list comprehension to create a new list with only positive numbers
+pos_numbers = [num for num in numbers if num > 0]
+print("Positive numbers: ", pos_numbers)
