1f35afa491e46e2180c28ab60c92a72ecb3c3ec3
ZoraPhoenix <raisondetre2001@gmail.com>
2025-04-04 16:26:59 -0400

    Create dictionary and add multiple contacts

---
+++ Dictionaries_P.py
@@ -0,0 +1,14 @@
+# Dictionaries
+
+# A dictionary is a collection of key-value pairs. Each key is unique and maps to a value.
+phonebook = {}
+phonebook["Zora"] = "123-456-7890"
+print(phonebook["Zora"])
+
+# Let's add a few more entries to the phonebook
+phonebook["Robert"] = "234-567-8901"
+phonebook["Alice"] = "345-678-9012"
+phonebook["Bob"] = "456-789-0123"
+
+print(phonebook)
+
